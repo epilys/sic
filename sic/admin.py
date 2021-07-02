38611e305ee24925dbc2557c0dd504cbc8578d36
Manos Pitsidianakis <el13635@mail.ntua.gr>
2021-07-02 09:16:18 +0300

    Add missing models to admin.py

--- sic/admin.py
+++ sic/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(User)
-admin.site.register(Story)
 admin.site.register(Comment)
+admin.site.register(Domain)
 admin.site.register(Hat)
+admin.site.register(Invitation)
 admin.site.register(Message)
+admin.site.register(Moderation)
+admin.site.register(Story)
 admin.site.register(Tag)
+admin.site.register(TagFilter)
+admin.site.register(User)
+admin.site.register(Vote)
