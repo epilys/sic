253eb9707ad12b5bf4acde50c584b72d6d3554fc
Manos Pitsidianakis <el13635@mail.ntua.gr>
2021-07-05 14:20:19 +0300

    Add html validation tool and validate/format doco

---
+++ tools/recursive_html_validate.py
@@ -0,0 +1,227 @@
+from bs4 import BeautifulSoup
+import argparse
+import sys
+import urllib.parse
+import urllib.request
+import http.cookiejar
+from subprocess import Popen, PIPE
+
+"""
+Adapted from https://gist.github.com/epilys/b78fe285a2647ece8689f7c7c1bca90c
+
+TODO: BeautifulSoup not needed, we can use python's HTMLParser instead.
+
+default behaviours:
+
+- looks for "vnu.jar" in PATH, but you can override that with --vnu-jar-bin flag.
+- doesn't login, use --login with --username and --password for that
+- doesn't check syndication feeds (rss, atom)
+
+invocation: python3 tools/recursive_html_validate.py
+
+usage: recursive_html_validate.py [-h] [--login] [--check-xml]
+                                  [--vnu-jar-bin VNU_JAR_BIN] [-u USERNAME]
+                                  [-p PASSWORD]
+                                  url
+
+positional arguments:
+  url
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --login               login before scraping? (html and valid endpoints are
+                        different when authenticated
+  --check-xml           also check XML (feeds)
+  --vnu-jar-bin VNU_JAR_BIN
+  -u USERNAME, --username USERNAME
+  -p PASSWORD, --password PASSWORD
+"""
+
+AUTHENTICATION_URL = "/admin/login/"
+
+
+def scrape(
+    username,
+    password,
+    vnu_jar_bin="vnu.jar",
+    root_url="127.0.0.1:8002",
+    _login=True,
+    check_xml=False,
+):
+    ROOT_URL = root_url
+    HTTP_ROOT_URL = "http://" + ROOT_URL
+    CHECK_XML = check_xml
+    VNU_CMD = [vnu_jar_bin, "-"]
+    VNU_CMD_XML = [vnu_jar_bin, "--format", "xml", "-"]
+
+    # username and password for login
+    USERNAME = username
+    PASSWORD = password
+    # here goes URL that's found inside form action='.....'
+    #   adjust as needed, can be all kinds of weird stuff
+
+    # initiate the cookie jar (using : http.cookiejar and urllib.request)
+    cookie_jar = http.cookiejar.CookieJar()
+    opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(cookie_jar))
+    urllib.request.install_opener(opener)
+    urls = []
+
+    def make_get(url):
+        ####### change variables here, like URL, action URL, user, pass
+        # your base URL here, will be used for headers and such, with and without https://
+        base_url = ROOT_URL
+        https_base_url = HTTP_ROOT_URL
+        url = https_base_url + url
+
+        ####### rest of the script is logic
+        # but you will need to tweak couple things maybe regarding "token" logic
+        #   (can be _token or token or _token_ or secret ... etc)
+
+        # big thing! you need a referer for most pages! and correct headers are the key
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "User-agent": "Mozilla/5.0 Chrome/81.0.4044.92",  # Chrome 80+ as per web search
+            "Host": base_url,
+            "Origin": https_base_url,
+            "Referer": https_base_url,
+        }
+        # first a simple request, just to get login page and parse out the token
+        #       (using : urllib.request)
+        request = urllib.request.Request(url)
+        response = urllib.request.urlopen(request)
+        contents = response.read()
+
+        html = contents.decode("utf-8")
+        return html
+
+    def make_post(url, payload):
+        ####### change variables here, like URL, action URL, user, pass
+        # your base URL here, will be used for headers and such, with and without https://
+        base_url = ROOT_URL
+        https_base_url = HTTP_ROOT_URL
+        url = https_base_url + url
+
+        ####### rest of the script is logic
+        # but you will need to tweak couple things maybe regarding "token" logic
+        #   (can be _token or token or _token_ or secret ... etc)
+
+        # big thing! you need a referer for most pages! and correct headers are the key
+        headers = {
+            "Content-Type": "application/x-www-form-urlencoded",
+            "User-agent": "Mozilla/5.0 Chrome/81.0.4044.92",  # Chrome 80+ as per web search
+            "Host": base_url,
+            "Origin": https_base_url,
+            "Referer": https_base_url,
+        }
+        # first a simple request, just to get login page and parse out the token
+        #       (using : urllib.request)
+        request = urllib.request.Request(url)
+        response = urllib.request.urlopen(request)
+        contents = response.read()
+
+        # parse the page, we look for token eg. on my page it was something like this:
+        #    <input type="hidden" name="_token" value="random1234567890qwertzstring">
+        #       this can probably be done better with regex and similar
+        #       but I'm newb, so bear with me
+        html = contents.decode("utf-8")
+        # text just before start and just after end of your token string
+        mark_start = '<input type="hidden" name="csrfmiddlewaretoken" value="'
+        mark_end = '">'
+        # index of those two points
+        start_index = html.find(mark_start) + len(mark_start)
+        end_index = html.find(mark_end, start_index)
+        # and text between them is our token, store it for second step of actual login
+        token = html[start_index:end_index]
+
+        # here we craft our payload, it's all the form fields, including HIDDEN fields!
+        #   that includes token we scraped earler, as that's usually in hidden fields
+        #   make sure left side is from "name" attributes of the form,
+        #       and right side is what you want to post as "value"
+        #   and for hidden fields make sure you replicate the expected answer,
+        #       eg. "token" or "yes I agree" checkboxes and such
+
+        payload["csrfmiddlewaretoken"] = token
+
+        # now we prepare all we need for login
+        #   data - with our payload (user/pass/token) urlencoded and encoded as bytes
+        data = urllib.parse.urlencode(payload)
+        binary_data = data.encode("UTF-8")
+        # and put the URL + encoded data + correct headers into our POST request
+        #   btw, despite what I thought it is automatically treated as POST
+        #   I guess because of byte encoded data field you don't need to say it like this:
+        #       urllib.request.Request(authentication_url, binary_data, headers, method='POST')
+        request = urllib.request.Request(url, binary_data, headers)
+        response = urllib.request.urlopen(request)
+        contents = response.read()
+        contents = contents.decode("utf-8")
+        return contents
+
+    def login():
+        login_data = {
+            #'name':'value',    # make sure this is the format of all additional fields !
+            "username": USERNAME,
+            "password": PASSWORD,
+            "next": "/",
+        }
+        contents = make_post(AUTHENTICATION_URL, login_data)
+
+    def rec_scrape(site):
+        try:
+            r = make_get(site)
+        except Exception as exc:
+            print(site, exc, file=sys.stderr)
+            return False
+        print(HTTP_ROOT_URL + site)
+        vnu_cmd = VNU_CMD
+        if site.endswith(".rss") or site.endswith(".atom"):
+            if not CHECK_XML:
+                print(site, "check_xml is false, ignoring", file=sys.stderr)
+                return
+            vnu_cmd = VNU_CMD_XML
+        with Popen(vnu_cmd, stdin=PIPE) as proc:
+            proc.stdin.write(bytes(r, "utf-8"))
+        s = BeautifulSoup(r, "html.parser")
+
+        for i in s.find_all("a"):
+            href = i.attrs["href"]
+            if href.startswith("/"):
+                site = href
+                if site not in urls and not site.startswith("http"):
+                    urls.append(site)
+                    rec_scrape(site)
+        return True
+
+    if _login:
+        login()
+    rec_scrape("/")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument("url", type=str, help="")
+    parser.add_argument(
+        "--login",
+        action="store_true",
+        default=False,
+        help="login before scraping? (html and valid endpoints are different when authenticated",
+    )
+    parser.add_argument(
+        "--check-xml",
+        action="store_true",
+        default=False,
+        help="also check XML (feeds)",
+    )
+    parser.add_argument("--vnu-jar-bin", type=str, default="vnu.jar")
+    parser.add_argument("-u", "--username", type=str, default=None)
+    parser.add_argument("-p", "--password", type=str, default=None)
+
+    args = parser.parse_args()
+
+    scrape(
+        args.username,
+        args.password,
+        vnu_jar_bin=args.vnu_jar_bin,
+        root_url=args.url,
+        _login=args.login,
+        check_xml=args.check_xml,
+    )
