d9ac0b447c67b9549439adcc253593a4f4f19daf
Manos Pitsidianakis <el13635@mail.ntua.gr>
2021-07-07 00:36:20 +0300

    Add url_with_next template tag

---
+++ sic/templatetags/utils.py
@@ -0,0 +1,54 @@
+from django import template
+from django.utils.http import urlencode
+from django.template.defaulttags import URLNode, url
+from django.template.exceptions import TemplateSyntaxError
+from django.template.base import Token, Node, kwarg_re
+
+register = template.Library()
+
+
+class URLNextNode(Node):
+    def __init__(self, url_node, request):
+        self.url_node = url_node
+        self.request = request
+
+    def __repr__(self):
+        return "<%s url_node=%s request=%s>" % (
+            self.__class__.__qualname__,
+            repr(self.url_node),
+            repr(self.request),
+        )
+
+    def render(self, context):
+        inner = self.url_node.render(context)
+        request = self.request.resolve(context)
+        full_path = request.get_full_path()
+        # prevent infinite loops for bots
+        _next = not full_path.startswith(inner)
+        _next_s = urlencode({"next": full_path})
+
+        return inner + ("?" + _next_s) if _next else ""
+
+
+@register.tag
+def url_with_next(parser, token):
+    bits = token.split_contents()
+    if len(bits) < 3:
+        raise TemplateSyntaxError(
+            "'%s' takes at least two arguments, a URL pattern name and the request."
+            % bits[0]
+        )
+    request = parser.compile_filter(bits[-1])
+    bits = ["url"] + bits[1:-1]
+    return URLNextNode(
+        url(
+            parser,
+            Token(
+                token.token_type,
+                " ".join(bits),
+                position=token.position,
+                lineno=token.lineno,
+            ),
+        ),
+        request,
+    )
