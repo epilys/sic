5330b0fbd7dd8bafe989858e422dee42b926e661
Manos Pitsidianakis <el13635@mail.ntua.gr>
2021-07-07 11:10:15 +0300

    Make tag name unique

--- sic/models.py
+++ sic/models.py
@@ -125,7 +125,7 @@ class Comment(models.Model):
 
 class Tag(models.Model):
     id = models.AutoField(primary_key=True)
-    name = models.CharField(null=False, blank=False, max_length=20)
+    name = models.CharField(null=False, blank=False, max_length=20, unique=True)
     hex_color = models.CharField(max_length=7, null=True, blank=True, default="#fffff")
     created = models.DateTimeField(auto_now_add=True)
     parents = models.ManyToManyField("Tag", blank=True)
