70f9dd3eb8b698118db79f272a9b1bec32dd0d08
Manos Pitsidianakis <el13635@mail.ntua.gr>
2021-07-22 11:15:56 +0300

    Redirect to index after logout

--- sic/urls.py
+++ sic/urls.py
@@ -132,7 +132,11 @@ urlpatterns = [
         auth_views.LoginView.as_view(template_name="login.html"),
         name="login",
     ),
-    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
+    path(
+        "accounts/logout/",
+        auth_views.LogoutView.as_view(next_page="index"),
+        name="logout",
+    ),
     path(
         "accounts/password-reset/",
         auth_views.PasswordResetView.as_view(template_name="password_reset.html"),
