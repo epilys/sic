6c2218261e4590220bb8eb93fdf955e050c5d4f4
Manos Pitsidianakis <el13635@mail.ntua.gr>
2021-07-31 13:53:35 +0300

    feeds.py: add <comments> metadata

--- sic/feeds.py
+++ sic/feeds.py
@@ -1,6 +1,8 @@
+from django.contrib.sites.shortcuts import get_current_site
 from django.contrib.syndication.views import Feed
 from django.http import Http404, HttpResponseForbidden
-from django.utils.feedgenerator import Atom1Feed
+from django.utils.encoding import iri_to_uri
+from django.utils.feedgenerator import Atom1Feed, Rss201rev2Feed
 from django.views.decorators.http import require_http_methods
 from .models import Story, User
 from .auth import AuthToken
@@ -8,10 +10,39 @@ from .auth import AuthToken
 # Edit site domain in /admin/sites/site/1/change
 
 
-class LatestStoriesRss(Feed):
+# https://github.com/django/django/blob/fbb1984046ae00bdf0b894a6b63294395da1cce8/django/contrib/syndication/views.py#L13
+def add_domain(domain, url, secure=False):
+    protocol = "https" if secure else "http"
+    if url.startswith("//"):
+        # Support network-path reference (see #16753) - RSS requires a protocol
+        url = "%s:%s" % (protocol, url)
+    elif not url.startswith(("http://", "https://", "mailto:")):
+        url = iri_to_uri("%s://%s%s" % (protocol, domain, url))
+    return url
+
+
+class RssFeed(Rss201rev2Feed):
+    def add_item(self, *args, **kwargs):
+        if "_comments" in kwargs:
+            kwargs["comments"] = kwargs["_comments"]
+        return super().add_item(*args, **kwargs)
+
+
+class AtomFeed(Atom1Feed):
+    def add_item(self, *args, **kwargs):
+        if "_comments" in kwargs:
+            kwargs["comments"] = kwargs["_comments"]
+        return super().add_item(*args, **kwargs)
+
+
+class LatestStories(Feed):
     title = "sic latest stories"
     link = "/"
     description = ""
+
+    def __init__(self, *args, **kwargs):
+        self.request = None
+        super().__init__(*args, **kwargs)
 
     def items(self):
         return Story.objects.order_by("-created")[:10]
@@ -32,12 +63,45 @@ class LatestStoriesRss(Feed):
         return map(lambda t: str(t), item.tags.all())
 
     def item_link(self, item):
-        return item.get_absolute_url()
+        return item.get_listing_url()
+
+    def get_context_data(self, **kwargs):
+        # Bit of a hack, get_context_data() is called by the Feed view in
+        # django/contrib/syndication/views.py so store the request object in
+        # order to retrieve the domain from it for the comments url in
+        # item_extra_kwargs()
+        if "request" in kwargs:
+            self.request = kwargs.get("request")
+        return super().get_context_data(**kwargs)
+
+    def item_extra_kwargs(self, item):
+        link = self.item_link(item)
+        url = item.get_absolute_url()
+        if link != url:
+            if self.request is not None:
+                current_site = get_current_site(self.request)
+                return {
+                    "_comments": add_domain(
+                        current_site.domain,
+                        url,
+                        self.request.is_secure(),
+                    )
+                }
+            return {
+                "_comments": url,
+            }
+        else:
+            return {}
 
 
-class LatestStoriesAtom(LatestStoriesRss):
-    feed_type = Atom1Feed
-    subtitle = LatestStoriesRss.description
+class LatestStoriesRss(LatestStories):
+    feed_type = RssFeed
+    subtitle = LatestStories.description
+
+
+class LatestStoriesAtom(LatestStories):
+    feed_type = AtomFeed
+    subtitle = LatestStories.description
 
 
 class UserLatestStoriesFeed(Feed):
