daa78ea6ce5ecbb76ab172080e818e36b9f13c8c
Manos Pitsidianakis <el13635@mail.ntua.gr>
2021-11-19 17:53:33 +0200

    Add /t/ markdown tag, story markdown tag

--- sic/markdown.py
+++ sic/markdown.py
@@ -3,22 +3,26 @@ import re
 from markdown_it import MarkdownIt
 from markdown_it.rules_inline import StateInline
 from django.utils.safestring import mark_safe
-from django.urls import reverse
+from django.urls import reverse, resolve
 
 
 def link_render(self, tokens, idx, options, env):
     return self.renderToken(tokens, idx, options, env)
 
 
-def make_link_rule(tag: str, url_fn, exists_fn):
+def make_link_rule(tag: str, url_fn, exists_fn, detect_fn=None):
+    if detect_fn is None:
+        detect_fn = lambda t: len(f"</{tag}") if t.startswith(f"</{tag}/") else False
+
     def _func(markdown):
         def link_def(state: StateInline, _silent: bool) -> bool:
             pos = state.pos
 
-            if not state.src[pos:].startswith(f"</{tag}/"):
+            detect = detect_fn(state.src[pos:])
+            if not detect:
                 return False
+            start = state.pos + detect
 
-            start = state.pos + len(f"</{tag}")
             maximum = state.posMax
 
             objname = None
@@ -35,8 +39,9 @@ def make_link_rule(tag: str, url_fn, exists_fn):
                     break
 
             objname = state.src[start + 1 : pos]
+            exists = exists_fn(objname)
 
-            if not exists_fn(objname):
+            if not exists:
                 return False
 
             target_url = url_fn(objname)
@@ -49,25 +54,22 @@ def make_link_rule(tag: str, url_fn, exists_fn):
             token.info = "auto"
 
             token = state.push("text", "", 0)
-            token.content = f"/{tag}/" + objname
+            if isinstance(exists, str):
+                token.content = exists
+            else:
+                token.content = f"/{tag}/" + objname
 
             token = state.push(f"{tag}_link_close", "a", -1)
             token.markup = f"{tag}_link"
             token.info = "auto"
 
-            state.pos += len(objname) + len(f"</{tag}/>")
+            state.pos = pos + len(">")
             return True
 
         markdown.inline.ruler.after("autolink", f"{tag}_link_def", link_def)
         markdown.add_render_rule(f"{tag}_link_open", link_render, fmt="html")
 
     return _func
-
-
-# def tag_exists(name):
-#    from .models import Tag
-#
-#    return Tag.objects.filter(name=name).exists()
 
 
 def user_exists(username):
@@ -77,12 +79,56 @@ def user_exists(username):
 
 
 user_link = make_link_rule(
-    "u", lambda username: reverse("profile", kwargs={"username": username}), user_exists
+    "u", lambda username: reverse("profile", kwargs={"name": username}), user_exists
+)
+
+
+def tag_exists(name):
+    from .models import Tag
+
+    tag = Tag.objects.filter(name=name)
+    if not tag.exists():
+        return False
+    return tag.first().name
+
+
+def tag_url(name):
+    from .models import Tag
+
+    return Tag.objects.filter(name=name).first().get_absolute_url()
+
+
+tag_link = make_link_rule("t", tag_url, tag_exists)
+
+
+def story_exists(story_url):
+    from .models import Story
+    from .views import stories
+
+    try:
+        func, args, kwargs = resolve(f"/{story_url}")
+        if func == stories.story:
+            story_obj = Story.objects.get(pk=kwargs["story_pk"])
+            return story_obj.title
+        return False
+    except Exception:
+        return False
+
+
+story_link = make_link_rule(
+    "story_tag",
+    lambda story_url: story_url,
+    story_exists,
+    detect_fn=lambda t: 1 if t.startswith("</") else False,
 )
 # tag_link = make_link_rule("t", lambda name: f"/t/{name}", tag_exists)
 
 MarkdownRenderer = (
-    MarkdownIt("gfm-like", {"html": False}).enable(["linkify"]).use(user_link)
+    MarkdownIt("gfm-like", {"html": False})
+    .enable(["linkify"])
+    .use(user_link)
+    .use(tag_link)
+    .use(story_link)
 )
 
 
